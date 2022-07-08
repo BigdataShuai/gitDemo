e0e1ca0ab66a6fe89f624e866a2f7c4c78b99f37
BigdataShuai <shuai414527@163.com>
2022-07-08 14:20:28 +0800

    third commit

--- src/main/java/gits.java
+++ src/main/java/gits.java
@@ -2,5 +2,6 @@ public class gits {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("second");
+        System.out.println("three");
     }
 }
